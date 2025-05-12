caea1bf5641dc1be5054c30cd9edeb880c958e1e
Diogo Marques <1462675@sga.pucminas.br>
2025-05-12 16:41:37 -0300

    iniciando projeto

---
+++ Pesquisa pokemon/Pokemon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pesquisa_pokemon
+{
+    internal class Pokemon
+    {
+        public string  Nome { get; set; }
+        public  int Altura { get; set; }
+        public int Peso { get; set; }
+        public List<string> Tipos { get; set; } = new();
+        public List<string> Golpes { get; set; } = new();
+
+
+        public void ExibirPokemon()
+        {
+            Console.WriteLine("Nome:{0}\n" +
+                "Altura: {1}\n" +
+                "Peso: {2}", Nome, Altura,Peso);
+            Console.WriteLine("Tipos: " + string.Join(", ", Tipos));
+            Console.WriteLine("Golpes: " + string.Join(", ", Golpes));
+        }
+    }
+}
---
+++ Pesquisa pokemon/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MeuProjetoPokemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem-vindo ao PokéApp!");
+
+        }
+    }
+}
